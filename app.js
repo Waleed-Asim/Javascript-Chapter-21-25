444eb7ec849bf90bf091f084e9448866fbb2c1bb
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-01 02:28:32 +0500

    Add initial files

---
+++ app.js
@@ -0,0 +1,125 @@
+// ***********************Chap 21-25***************************
+
+// Q1
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+
+// var fullName = (firstName + " " + lastName);
+
+// console.log("Welcome " + fullName);
+
+// Q2
+
+// var favPhone = prompt("What is your favorite mobile phone model?");
+
+// console.log("My favorite phone is " + favPhone);
+// console.log("Length of string is "  + favPhone.length);
+
+
+// Q3
+
+// var nation = "Pakistan";
+
+// console.log("String: " + nation);
+// console.log("Index of 'n' : "  + nation.indexOf("n"));
+
+// Q4
+
+// var name = "Hello World";
+
+// var Oname = name.indexOf(name.slice(9));
+
+// console.log("String: " + name);
+// console.log("Last index of 'l' is : " +  Oname);
+
+// Q5
+
+
+// var pak = "Pakistan";
+
+// console.log("String: " + pak);
+// console.log("Character at index 3 is : " +  pak.charAt(3));
+
+
+// Q6
+
+// Contact method ??
+
+
+// Q7
+
+// var hyd = "Hyderabad";
+
+// var isl = hyd.replace("Hyder", "Islam");
+
+// console.log("City: " + hyd);
+// console.log("After replacement : " +  isl);
+
+// Q8
+
+// var msg = "Ali and Sami are best friends. They play cricket and football together.";
+
+// var newMsg = msg.replace(/and/g, "&");
+
+// console.log(newMsg)
+
+// Q9  ???????
+
+// 10
+
+// var name = "peanuts";
+// var capName = name.toUpperCase();
+
+// console.log("User input: " + name);
+// console.log("Upper case: " + capName);
+
+// Q11
+
+// var java = prompt("Write javascript");
+// var capName = java.charAt(0).toUpperCase() + java.slice(1);
+
+// console.log("User input: " + java);
+// console.log("Title case: " + capName);
+
+// Q12
+
+// var num = 35.36;
+
+// var stringOf = num.toString();
+
+// var newString = stringOf.slice(0,2) + stringOf.slice(3);
+
+// console.log("Number: " + num);
+// console.log("String: " + newString);
+
+// // Q13 
+
+
+// Q14 need to add condition
+
+// var arr = ["cake" , "apple" , "pie" , "cookie" , "chips" , "patties"];
+
+// var user = prompt("Welcome to my bakery what do you want to order?");
+
+// console.log(user.toLowerCase() + " is available at " + arr.indexOf(user) + " in our bakery");
+
+
+//  Q16
+
+// var string = "Univeristy of Karachi";
+
+// var split = string.split("");
+
+// console.log(split)
+
+
+
+
+
+
+
+
+
+
+
